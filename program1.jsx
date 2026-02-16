123f18111a8ee89d9fa2793ed0ae4fa5b4c7e91e
Subasree-27 <suba272006@gmail.com>
2026-02-16 10:00:06 +0530

    Add Counter component with increment, decrement, and reset

---
+++ program1.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+function Counter() {
+    const [count,setcount]=useState(0)
+    const incre=()=>{
+        setcount(count+1)
+    }
+    const decre=()=>{
+        setcount(count-1)
+    }
+    const reset=()=>{
+        setcount(0)
+    }
+  return (
+    <div>
+      <h1>Counter-App</h1>
+      <p>{count}</p>
+      <button onClick={incre}>Increment</button>
+      <button onClick={decre}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+
+    </div>
+  )
+}
+
+export default Counter
